3109f734b9a4370b8885c45da26b6ef2eaf923ea
Vincente67 <vincente_allan@paraparaumucollege.school.nz>
2026-05-04 14:12:06 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello ")
+def calculate_resistor():
+    print ("LED Resistor Calculator")
+
+    try:
+        # Input 
+        supply_voltage = float(input(" Enter supply voltage (V): "))
+        led_voltage = float(input("Enter LED foward voltage (V): "))
+        current = float(input("Enter current (A): "))
+      
+        # Validation 
+        if supply_voltage <= 0 or led_voltage <= 0 or current <=0:
+        print(values must be greater than 0")
+        return 
+
+        if led_voltage >= supply_voltage:
+        print ("LED Voltage must be less than supply voltage")
+        return 
