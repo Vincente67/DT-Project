60b8b2390e2b74415e64190ee324e59b18947818
Vincente67 <vincente_allan@paraparaumucollege.school.nz>
2026-05-11 20:10:37 +1200

    Fix quiz questions and formatting in main.py

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ def calculate_resistor():
 
         # Output 
         print(f"\nRequired resistor: {round(resistance, 2)} ohms")
-
-    #------------------- Quiz selection --------------------------
         
 
-        print("\n --- Ohm's law quiz ---")
-        print("What is the formula for Ohm's law?")
+    #-------------------Practice Question 1 --------------------------
+        print("\n--- Ohm's law quiz ---")
+        print("What is the formula for Ohm's Law?")
         print("A. V = I x R")
         print("B. V = I + R")
         print("B. V = I + R") 
@@ -38,6 +37,34 @@ def calculate_resistor():
             print("Correct! Ohm's law is V = I x R")
         else:
             print("Incorrect. The correct answer was V = I x R")
+
+
+    #------------------- Practice Question 2 --------------------------
+        print("\n--- Ohm's law quiz 2 ---")
+        print("What does 'R' stand for in Ohm's Law?)
+        print("A. Resistance")
+        print("B. Rotation")
+        print("C. Radiation")
+            
+       answer2 = input("Enter A, B, or C: ").lower()
+       if answer2 == "a":
+           print("Correct! R stands for Resistance")
+       else:
+           print ("Incorrect. The correct answer is A. Resistance")
+
+           
+    #------------------- Practice Question 3 --------------------------
+        print("\n--- Ohm's law quiz 3 ---")
+        print("What unit is resistance measured in?")
+        print("A. Volts")
+        print("B. Amps")
+        print("C. Ohms")
+
+        answer3 = input("Enter A, B, or C: ").lower()
+        if answer3 == "c"
+            print("Correct! Resistance is measured in ohms")
+        else:
+            print("Incorrect. The correct answer is C. Ohms")
 
     
    except ValueError:
